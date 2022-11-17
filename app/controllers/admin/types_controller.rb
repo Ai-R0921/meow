0f6dadf1990750c6f57d89358a442c1bbd8fc2d5
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-11-17 14:30:49 +0000

    [Finish]type登録、削除機能実装

--- app/controllers/admin/types_controller.rb
+++ app/controllers/admin/types_controller.rb
@@ -2,18 +2,24 @@ class Admin::TypesController < ApplicationController
   def new
     @type = Type.new
   end
-  
+
   def create
     @type = Type.new(type_params)
+    @type.save
+    redirect_to admin_types_path
   end
 
   def destroy
+    @type = Type.find(params[:id])
+    @type.destroy
+    redirect_to admin_types_path
   end
 
   def index
+    @types = Type.all
   end
-  
-  
+
+
   def type_params
     params.require(:type).permit(:name)
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when User
+      root_path
+    when Admin
+      admin_top_path
+    end
+  end
+
 end
