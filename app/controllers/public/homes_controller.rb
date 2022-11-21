0656711c89e713570ea3e0a8467d447098d2d94a
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-11-21 14:31:16 +0000

    [WIP]各リンク実装中

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,5 @@
 class Public::HomesController < ApplicationController
   def top
+    @post_images = PostImage.order('id DESC').limit(6)
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,11 +1,16 @@
 class Public::UsersController < ApplicationController
   def show
+    @user = current_user
   end
 
   def edit
+    @user = current_user
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path
   end
 
   def unsubscribe
@@ -13,4 +18,11 @@ class Public::UsersController < ApplicationController
 
   def withdraw
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :profile_image, :email, :is_deleted, :encrypted_password)
+  end
+
 end
