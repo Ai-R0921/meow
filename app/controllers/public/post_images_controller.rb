3636f02022164d6d2fd9fc6dac7bf008f9ba28c8
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-12-06 14:31:17 +0000

    [Finish]post_lost_catキーワード検索実装

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -42,11 +42,40 @@ class Public::PostImagesController < ApplicationController
 
   def search
     @section_title = "「#{params[:search]}」の検索結果"
-    @post_images = if params[:search].present?
-                    PostImage.where('title LIKE ?', "%#{params[:search]}%")
-                  else
-                    @post_images = PostImage.all
-                  end
+
+    # PostImageをキーワード検索
+    if params[:search].present?
+      post_images = PostImage.where('title LIKE ?', "%#{params[:search]}%").or(PostImage.where('body LIKE ?', "%#{params[:search]}%"))
+    else
+      post_images = PostImage.all
+    end
+
+    # PostLostCatをキーワード検索
+    if params[:search].present?
+      post_lost_cats = PostLostCat.where('title LIKE ?', "%#{params[:search]}%").or(PostLostCat.where('body LIKE ?', "%#{params[:search]}%"))
+    else
+      post_lost_cats = PostLostCat.all
+    end
+
+    # ハッシュ
+    # @post_imagesという空の箱を用意する
+    @post_images = {}
+    # 上の検索結果としてのpost_imagesをeachでひとつづつ取り出す
+    post_images.each do |post_image|
+      # @post_imagesにpost_imageを追加する
+      @post_images[post_image.created_at] = post_image
+    end
+    post_lost_cats.each do |post_lost_cat|
+      # @post_imagesにpost_lost_catを追加する
+      @post_images[post_lost_cat.created_at] = post_lost_cat
+    end
+
+    # 日付順で並び替えを行い、htmlでeachできる形に変換する
+    # { "2022-12-1" => post_image,"2022-11-25" => post_image,"2022-11-29" => post_lost_cat,}
+    # sortをすると{["2022-11-25" => post_image],["2022-11-29" => post_lost_cat],["2022-12-1" => post_image]}
+    # .to_h.valuesをすると[post_image,post_lost_cat,post_image]
+    @post_images = @post_images.sort.to_h.values
+    # 60-77行目までコピーしてタグ検索にも使用
   end
 
   private
