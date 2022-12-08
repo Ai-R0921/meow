a19a764c0f05301d25344064001068bf123197ef
Ai-R0921 <xoa1xo.0921@gmail.com>
2022-12-08 14:14:33 +0000

    [Finish]管理者側コメント管理機能実装

--- app/controllers/admin/post_image_comments_controller.rb
+++ app/controllers/admin/post_image_comments_controller.rb
@@ -1,7 +1,11 @@
 class Admin::PostImageCommentsController < ApplicationController
   def index
+    @post_image_comments = PostImageComment.all
   end
 
   def destroy
+    @post_image_comment = PostImageComment.find(params[:id])
+    @post_image_comment.destroy
+    redirect_to admin_post_image_comments_path
   end
 end
--- app/controllers/admin/post_lost_cats_controller.rb
+++ app/controllers/admin/post_lost_cats_controller.rb
@@ -1,10 +1,16 @@
 class Admin::PostLostCatsController < ApplicationController
   def index
+    @post_lost_cats = PostLostCat.all
   end
 
   def show
+    @post_lost_cat = PostLostCat.find(params[:id])
+    @user = @post_lost_cat.user
   end
 
   def destroy
+    @post_lost_cat = PostLostCat.find(params[:id])
+    @post_lost_cat.destroy
+    redirect_to admin_post_lost_cats_path
   end
 end
