1a3cb31d5bec05cd580aba5de82db24894fecbe1
Ai-R0921 <xoa1xo.0921@gmail.com>
2023-01-29 05:59:15 +0000

    [Add] seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(
-  email: 'abcd@hotmail.com',
-  password: '123456'
-  )
+# Admin.create!(
+#   email: 'abcd@hotmail.com',
+#   password: '123456'
+#   )
+
+
+admins = [
+  {email: 'abcd@hotmail.com', password: '123456'},
+]
+admins.each do |admin|
+  admin_data = Admin.find_by(email: admin[:email])
+  if admin_data.nil?
+    Admin.create(email: admin[:email], password: admin[:password])
+  end
+end
+
+types = ["アメリカンショートヘア",
+ "スコティッシュフォールド",
+ "ミヌエット",
+ "タイ",
+ "雑種",
+ "ブリティッシュショートヘア",
+ "ベンガル",
+ "マンチカン",
+ "エキゾチックショートヘア",
+ "シャム",
+ "ロシアンブルー",
+ "ペルシャ",
+ "三毛猫",
+ "メインクーン",
+ "ラグドール",
+ "サイベリアン",
+ "ノルウェージャンフォレストキャット",
+ "アメリカンカール",
+ "ソマリ",
+ "スフィンクス",
+ "アビシニアン",
+ "アメリカンボブテイル",
+ "オリエンタルショートヘア",
+ "ラガマフィン",
+ "ヒマラヤン",
+ "ブリティッシュロングヘア",
+ "その他",
+ "マンクス"
+]
+
+types.each do |type|
+  Type.find_or_create_by(name: type)
+end
