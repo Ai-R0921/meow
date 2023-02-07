97e86571cfab75e967277270d9843315d9fb621e
Ai-R0921 <xoa1xo.0921@gmail.com>
2023-02-07 16:22:05 +0000

    [Fix]いいね非同期中

--- app/controllers/public/post_image_favorites_controller.rb
+++ app/controllers/public/post_image_favorites_controller.rb
@@ -5,13 +5,17 @@ class Public::PostImageFavoritesController < ApplicationController
     post_image = PostImage.find(params[:post_image_id])
     post_image_favorite = current_user.post_image_favorites.new(post_image_id: post_image.id)
     post_image_favorite.save
-    redirect_to post_image_path(post_image)
+    # redirect_to post_image_path(post_image)
   end
 
   def destroy
     post_image = PostImage.find(params[:post_image_id])
     post_image_favorite = current_user.post_image_favorites.find_by(post_image_id: post_image.id)
     post_image_favorite.destroy
-    redirect_to post_image_path(post_image)
+    # redirect_to post_image_path(post_image)
+  end
+
+  def favorited_by?(user)
+     post_image_favorites.exists?(user_id: user.id)
   end
 end
