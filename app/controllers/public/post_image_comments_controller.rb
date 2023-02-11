5fbc8a8b575b7a78bf878c1a39ca777638b033cf
Ai-R0921 <xoa1xo.0921@gmail.com>
2023-02-11 17:06:21 +0000

    [Fix]コメント非同期化中

--- app/controllers/public/post_image_comments_controller.rb
+++ app/controllers/public/post_image_comments_controller.rb
@@ -4,17 +4,18 @@ class Public::PostImageCommentsController < ApplicationController
   before_action :prevent_url, only: [:destroy]
 
   def create
-    post_image = PostImage.find(params[:post_image_id])
+    @post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_image_comments.new(post_image_comment_params)
-    comment.post_image_id = post_image.id
+    comment.post_image_id = @post_image.id
     comment.save
-    redirect_to post_image_path(post_image)
+    # redirect_to post_image_path(post_image)
   end
 
   def destroy
+    @post_image = PostImage.find(params[:post_image_id])
     if @post_image_comment.user_id == current_user.id
       @post_image_comment.destroy
-      redirect_to post_image_path(params[:post_image_id])
+      # redirect_to post_image_path(params[:post_image_id])
     end
   end
 
